dff25b8a60ee511d10c1c63f42571ac69727fccb
wild-rydes-user-at-380037443321 <kreloup@gmail.com>
2024-02-19 02:53:43 +0100

    Jenkins Server + EKS Cluster

--- Jenkins Server/backend.tf
+++ Jenkins Server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cicd-terraform-eks-mkrela"
-    key    = "jenkins/terraform.tfstate"
+    key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
 }
