eee72985c48170daaf1b6058a2516ef82933ab1a
wild-rydes-user-at-380037443321 <kreloup@gmail.com>
2024-02-19 02:51:52 +0100

    deployment of EKS Cluster

---
+++ EKS Cluster/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Private Subnet CIDR"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Subnet CIDR"
+  type        = list(string)
+}
